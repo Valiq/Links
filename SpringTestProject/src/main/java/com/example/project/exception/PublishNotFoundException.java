bae741be5ae3a824b3aa1772aaebdb1185d8ca0f
Valentin <56139293+Valiq@users.noreply.github.com>
2022-11-29 15:54:57 +0300

    d

--- SpringTestProject/src/main/java/com/example/project/exception/PublishNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.exception;
-
-public class PublishNotFoundException extends Exception{
-
-    public PublishNotFoundException(String message){
-       super(message);
-    }
-
-}
