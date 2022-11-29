167b51b507ac4e6459e12f137ce849030f0b2ef3
Valentin <56139293+Valiq@users.noreply.github.com>
2022-11-29 15:52:04 +0300

    3

--- SpringTestProject/src/main/java/com/example/project/exception/BookAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.exception;
-
-public class BookAlreadyExistException extends Exception{
-    public BookAlreadyExistException(String message) {
-        super(message);
-    }
-}
